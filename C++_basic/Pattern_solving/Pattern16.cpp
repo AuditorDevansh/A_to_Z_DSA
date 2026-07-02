1456500d4abd56edb86d1d9d217809f03dd60671
Devansh Mishra <166271475+AuditorDevansh@users.noreply.github.com>
2026-07-02 17:59:48 +0530

    Add pattern16 function to print character pattern
    Implement pattern printing for given integer n.

---
+++ C++_basic/Pattern_solving/Pattern16.cpp
@@ -0,0 +1,24 @@
+// Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+// A
+// BB
+// CCC
+// DDDD
+// EEEEE
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+// Print the pattern in the function given to you.
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    void pattern16(int n) {
+            char start = 'A';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << start << " ";
+        }
+        start++;
+        cout << endl;
+    }
+}
+};
