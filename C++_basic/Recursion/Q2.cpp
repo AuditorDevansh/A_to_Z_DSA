ea15d770edd7033a2f9d954e4b921caf0848f8c7
Devansh Mishra <166271475+AuditorDevansh@users.noreply.github.com>
2026-07-19 22:32:36 +0530

    Implement recursive function to print numbers from N to 1

---
+++ C++_basic/Recursion/Q2.cpp
@@ -0,0 +1,16 @@
+// Q: Print N to 1 using Recursion
+// -------------------------------------------------------------------------------------------------------
+// Given an integer n, write a function to print all numbers from n to 1 (inclusive) using recursion.
+// You must not use any loops such as for, while, or do-while.
+// The function should print each number on a separate line, in decreasing order from n to 1
+// --------------------------------------------------------------------------------------------------------
+class Solution {
+  public:
+    void printNumbers(int n) {
+        if(n<1)
+        return;
+        cout<< n <<endl;
+        printNumbers(n-1);
+
+    }
+};
